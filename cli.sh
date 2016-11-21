cb8663a6a2fe4c85110e6636beecd992daf28507
Joey Nguyen <jnn193@gmail.com>
2016-11-21 01:57:44 -0600

    Added more setup to cli.sh

--- cli.sh
+++ cli.sh
@@ -9,6 +9,7 @@
 ###############################################################################
 # Git                                                                         #
 ###############################################################################
+
 if [ ! -d ~/git ]; then
   echo "Creating ~/git directory..."
   mkdir ~/git 
@@ -18,6 +19,17 @@ fi
 if [ ! -d ~/git/dotfiles ]; then
   echo "Cloning joeynguyen/dotfiles repo..."
   git clone https://github.com/joeynguyen/dotfiles.git ~/git/dotfiles
+fi
+
+# Create symlink for .gitignore_global from dotfiles repo
+if [ ! -L ~/.gitignore_global ]; then
+  if [ -f ~/.gitignore_global ]; then
+    mv ~/.gitignore_global ~/.gitignore_global.bak
+  fi
+  echo "Creating symlink for .globalgitignore..."
+  ln -s ~/git/dotfiles/gitignore_global ~/.gitignore_global
+  echo "Setting global gitignore file..."
+  git config --global core.excludesfile '~/.gitignore_global'
 fi
 
 ###############################################################################
@@ -32,20 +44,42 @@ fi
 
 # Create symlink for .zshrc from dotfiles repo
 if [ ! -L ~/.zshrc ]; then
-  echo "Create symlink for .zshrc..."
+  echo "Creating symlink for .zshrc..."
   if [ -f ~/.zshrc  ]; then
     mv ~/.zshrc ~/.zshrc.bak
   fi
   ln -s ~/git/dotfiles/zshrc ~/.zshrc
 fi
 
+# Create symlink for .profile from dotfiles repo
+if [ ! -L ~/.profile ]; then
+  if [ -f ~/.profile ]; then
+    mv ~/.profile ~/.profile.bak
+  fi
+  echo "Creating symlink for .profile..."
+  ln -s ~/git/dotfiles/profile ~/.profile
+fi
+
+###############################################################################
+# Tools                                                                       #
+###############################################################################
+
 # Create symlink for .warpc from dotfiles repo
 if [ ! -L ~/.warprc ]; then
   if [ -f ~/.warprc ]; then
     mv ~/.warprc ~/.warprc.bak
   fi
-  echo "Create symlink for .warpc..."
+  echo "Creating symlink for .warpc..."
   ln -s ~/git/dotfiles/warprc ~/.warprc
+fi
+
+# Create symlink for .ackrc from dotfiles repo
+if [ ! -L ~/.ackrc ]; then
+  if [ -f ~/.ackrc ]; then
+    mv ~/.ackrc ~/.ackrc.bak
+  fi
+  echo "Creating symlink for .ackrc..."
+  ln -s ~/git/dotfiles/ackrc ~/.ackrc
 fi
 
 ###############################################################################
@@ -54,7 +88,7 @@ fi
 
 # Create symlink for neovim config file from dotfiles repo
 if [ ! -L ~/.config/nvim/init.vim ]; then
-  echo "Create symlink for neovim config file..."
+  echo "Creating symlink for neovim config file..."
   if [ ! -d ~/.config/nvim ]; then
     mkdir -p ~/.config/nvim
   fi
@@ -62,4 +96,10 @@ if [ ! -L ~/.config/nvim/init.vim ]; then
     mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
   fi
   ln -s ~/git/dotfiles/nvimrc ~/.config/nvim/init.vim
+fi
+
+# Fix for Neovim issue with Ctrl-h not being able to be remapped.
+if [ ! -f ~/$TERM.ti  ]; then
+  echo "Fix Neovim issue with Ctrl-h not being able to be remapped"
+  infocmp $TERM | sed 's/kbs=^[hH]/kbs=\\177/' > ~/$TERM.ti && tic $TERM.ti
 fi
